0668f9370c99213be06157f995a89d7e684188e0
Joseph Kao <josephtkao@gmail.com>
2020-08-06 16:34:18 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,6 +1,15 @@
 class Pirate
   attr_accessor :name, :weight, :height
+  @@all =[]
   def initialize(attrs)
-    attrs.each{|k,v| self.send("#{k}=",v)}
+    @name = params[pirate[:name]]
+    @weight = params[pirate[:weight]]
+    @height = params[pirate[:height]]
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 end
